7efc337d94523c22a6e39c109600c59810475b3f
Gyan Prakash <gyanprakash.gp98010@gmail.com>
2020-09-10 12:13:09 +0530

    jj

--- src/index.js
+++ src/index.js
@@ -12,6 +12,102 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get('/',(req,res)=>{
+    res.send("Hello world!");
+})
+app.post('/add',(req,res)=>{
+    const {num1,num2}=req.body;
+    if(typeof num1==='string' || typeof num2==='string'){
+        return res.status(400).json({
+            status:"error",
+            message:"invalid data types"
+        })
+    }
+    if(typeof num1==='number' && typeof num2==='number'){
+        if(num1>Number.MAX_VALUE || num2>Number.MAX_VALUE ){
+            return res.status(400).json({
+                status:`error`,
+                message:`Overflow`
+            })
+        }else{
+            return res.json({
+                status:`success`,
+                message:`the sum of given two number is`,
+                sum:`${num1}+${num2}`
+            })
+        }
+    }
+});
+app.post('/sub',(req,res)=>{
+    const {num1,num2}=req.body;
+    if(typeof num1==='string' || typeof num2==='string'){
+        return res.status(400).json({
+            status:"error",
+            message:"invalid data types"
+        })
+    }
+    if(typeof num1==='number' && typeof num2==='number'){
+        if(num1<Number.MIN_VALUE || num2<MIN_VALUE ){
+            return res.status(400).json({
+                status:`error`,
+                message:`Underflow`
+            })
+        }else{
+            return res.json({
+                status:`success`,
+                message:`the difference of given two number`,
+                sum:`${num1}-${num2}`
+            })
+        }
+    }
+});
+app.post('/multiply',(req,res)=>{
+    const {num1,num2}=req.body;
+    if(typeof num1==='string' || typeof num2==='string'){
+        return res.status(400).json({
+            status:"error",
+            message:"invalid data types"
+        })
+    }
+    if(typeof num1==='number' && typeof num2==='number'){
+        if(num1>Number.MAX_VALUE || num2>Number.MAX_VALUE ){
+            return res.status(400).json({
+                status:`error`,
+                message:`Overflow`
+            })
+        }else{
+            return res.json({
+                status:`success`,
+                message:`The product of given numbers`,
+                sum:`${num1}*${num2}`
+            })
+        }
+    }
+});
+app.post('/division',(req,res)=>{
+    const {num1,num2}=req.body;
+    if(typeof num1==='string' || typeof num2==='string'){
+        return res.status(400).json({
+            status:"error",
+            message:"invalid data types"
+        })
+    }
+    if(typeof num1==='number' && typeof num2==='number'){
+        if( num2==0 ){
+            return res.status(400).json({
+                status:`error`,
+                message:`Cannot divide by zero`
+            })
+        }else{
+            return res.json({
+                status:`success`,
+                message:`the sum of given two number is`,
+                sum:`${num1}/${num2}`
+            })
+        }
+    }
+});
+
 
 // here
 
